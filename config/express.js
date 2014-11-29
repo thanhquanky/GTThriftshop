cad1e3af745bfcc410a8ee971587a97df528b381
CastroFlavio <facj3@gatech.edu>
2014-11-29 14:12:42 -0500

    Increasing maximum size for uploaded files

--- config/express.js
+++ config/express.js
@@ -76,7 +76,8 @@ module.exports = function(db) {
 	app.use(bodyParser.urlencoded({
 		extended: true
 	}));
-	app.use(bodyParser.json());
+	app.use(bodyParser.json({limit: '50mb'}));
+        app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 	app.use(methodOverride());
 
 	// Enable jsonp
